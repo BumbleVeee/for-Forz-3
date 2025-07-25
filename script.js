17baabe85d59b8427b403d1f53de2806c822e10b
BumbleVeee <gubek.veronika.eva@gmail.com>
2025-07-25 19:39:33 +0200

    Update script.js
    For Forz <3

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
   const cake = document.querySelector(".cake");
   const candleCountDisplay = document.getElementById("candleCount");
-  let candles = [];
+  let candles = [
+  { x: 194.5, y: 19.5 },
+  { x: 197.5, y: 39.5 },
+  { x: 205.5, y: 47.5 },
+  { x: 28.5, y: 21.5 },
+  { x: 28.5, y: 38.5 },
+  { x: 110, y: 50 },
+  { x: 26.5, y: 47.5 },
+  { x: 191.5, y: 27.5 },
+  { x: 227.5, y: 11.5 },
+  { x: 222.5, y: 28.5 },
+  { x: 215.5, y: 43.5 },
+  { x: 114.5, y: 18.5 },
+  { x: 100.5, y: 3.5 },
+  { x: 82.5, y: 2.5 },
+  { x: 65.5, y: 10.5 },
+  { x: 64.5, y: 41.5 },
+  { x: 78.5, y: 64.5 },
+  { x: 94.5, y: 77.5 },
+  { x: 112.5, y: 90.5 },
+  { x: 129.5, y: 21.5 },
+  { x: 139.5, y: 5.5 },
+  { x: 155.5, y: 4.5 },
+  { x: 168.5, y: 10.5 },
+  { x: 168.5, y: 38.5 },
+  { x: 156.5, y: 62.5 },
+  { x: 146.5, y: 76.5 },
+  { x: 133.5, y: 88.5 },
+  { x: 122.5, y: 100.5 },
+  { x: 70.5, y: 62.5 },
+  { x: 167.5, y: 60.5 },
+  { x: 99.5, y: 87.5 },
+  ];
   let audioContext;
   let analyser;
   let microphone;
